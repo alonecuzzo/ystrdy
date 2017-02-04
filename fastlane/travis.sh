4eedd3290f654f280c30ed8cc61e9e9fc33b4ec5
Jabari Bell <jabari.bell@pxlflu.net>
2017-02-04 12:14:08 -0500

    updated carthage to only do iOS

--- fastlane/travis.sh
+++ fastlane/travis.sh
@@ -3,7 +3,7 @@
 if [[ "$TRAVIS_PULL_REQUEST" != "false" ]]; then
   gem update fastlane
   # carthage update --use-ssh --platform iOS
-  carthage bootstrap
+  carthage bootstrap --platform iOS
   fastlane test
   exit $?
 fi
