a71aefee0af484c22f2c49770d39e3f77dba5d92
Jabari Bell <jabari.bell@pxlflu.net>
2017-02-06 15:29:49 -0500

    three initial colors in

---
+++ ystrdy/Common/UIColor+Ystrdy.swift
@@ -0,0 +1,26 @@
+//
+//  UIColor+Ystrdy.swift
+//  ystrdy
+//
+//  Created by Jabari Bell on 2/6/17.
+//  Copyright © 2017 theowl. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+
+extension UIColor {
+    
+    static func ystrdyDarkPurple() -> UIColor {
+        return UIColor(red:0.21, green:0.18, blue:0.30, alpha:1.0)
+    }
+    
+    static func ystrdyWarm() -> UIColor {
+        return UIColor(red:0.77, green:0.31, blue:0.31, alpha:1.0)
+    }
+    
+    static func ystrdyCool() -> UIColor {
+        return UIColor(red:0.08, green:0.52, blue:0.94, alpha:1.0)
+    }
+}
--- ystrdy/ViewController.swift
+++ ystrdy/ViewController.swift
@@ -22,13 +22,16 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        view.backgroundColor = UIColor.ystrdyDarkPurple()
+        
         let nycCoordinates = CGPoint(x: 40.712784, y: -74.005941)
         let vm = WeatherDifferenceViewModel()
         vm.updateWeatherDifferenceForLocation(nycCoordinates)
         
         let label = UILabel(frame: .zero)
-        label.font = UIFont.systemFont(ofSize: 50)
+        label.font = UIFont.systemFont(ofSize: 100)
         label.textAlignment = .center
+        label.textColor = UIColor.ystrdyWarm()
         view.addSubview(label)
         label.snp.makeConstraints { make in
             make.center.equalTo(view)
