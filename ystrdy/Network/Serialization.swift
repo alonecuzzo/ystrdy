9375a665d6b0776124fbf02dd2351245993242d5
Jabari Bell <jabari.bell@pxlflu.net>
2017-02-06 13:57:47 -0500

    current temp mapping

---
+++ ystrdy/Network/Serialization.swift
@@ -0,0 +1,87 @@
+//
+//  Serialization.swift
+//  ystrdy
+//
+//  Created by Jabari Bell on 2/6/17.
+//  Copyright © 2017 theowl. All rights reserved.
+//
+
+import Foundation
+import Alamofire
+
+
+protocol LocalObjectSerializable {
+    init?(dictionary: [String: Any])
+    var dictionaryValue: [String: Any] { get }
+}
+
+protocol ResponseObjectSerializableType {}
+
+protocol ResponseObjectSerializable: ResponseObjectSerializableType {
+    init?(response: HTTPURLResponse, representation: Any)
+}
+
+extension DataRequest {
+    @discardableResult
+    func responseObject<T: ResponseObjectSerializable>(queue: DispatchQueue? = nil, completionHandler: @escaping (DataResponse<T>) -> Void) -> Self {
+        let responseSerializer = DataResponseSerializer<T> { request, response, data, error in
+            // TODO: HANDLE ERROR
+            
+            let jsonResponseSerializer = DataRequest.jsonResponseSerializer(options: .allowFragments)
+            let result = jsonResponseSerializer.serializeResponse(request, response, data, nil)
+            
+            guard case let .success(jsonObject) = result else {
+                fatalError("CRASH ON SERIALIZING")
+            }
+            
+            let responseObject = T(response: response!, representation: jsonObject)!
+            return .success(responseObject)
+        }
+        return response(queue: queue, responseSerializer: responseSerializer, completionHandler: completionHandler)
+    }
+}
+
+protocol ResponseCollectionSerializable {
+    static func collection(from response: HTTPURLResponse, withRepresentation: Any) -> [Self]
+}
+
+extension ResponseCollectionSerializable where Self: ResponseObjectSerializable {
+    static func collection(from response: HTTPURLResponse, withRepresentation representation: Any) -> [Self] {
+        var collection: [Self] = []
+        if let representation = representation as? [[String: Any]] {
+            for itemRepresentation in representation {
+                if let item = Self(response: response, representation: itemRepresentation) {
+                    collection.append(item)
+                }
+            }
+        }
+        return collection
+    }
+}
+
+extension DataRequest {
+    @discardableResult
+    func responseCollection<T: ResponseCollectionSerializable>(
+        queue: DispatchQueue? = nil,
+        completionHandler: @escaping (DataResponse<[T]>) -> Void) -> Self
+    {
+        let responseSerializer = DataResponseSerializer<[T]> { request, response, data, error in
+            //gotta handle errors
+            //guard error == nil else { return .failure(BackendError.network(error: error!)) }
+            let jsonSerializer = DataRequest.jsonResponseSerializer(options: .allowFragments)
+            let result = jsonSerializer.serializeResponse(request, response, data, nil)
+            
+            guard case let .success(jsonObject) = result else {
+                //return .failure(BackendError.jsonSerialization(error: result.error!))
+                fatalError("CRASH ON SERIALIZING")
+            }
+            // guard let response = response else {
+            //   let reason = "Response collection could not be serialized due to nil response."
+            // return .failure(BackendError.objectSerialization(reason: reason))
+            //}
+            return .success(T.collection(from: response!, withRepresentation: jsonObject))
+        }
+        
+        return response(responseSerializer: responseSerializer, completionHandler: completionHandler)
+    }
+}
--- ystrdy/ViewController.swift
+++ ystrdy/ViewController.swift
@@ -12,22 +12,79 @@ import RxSwift
 
 
 class ViewController: UIViewController {
+    
+    private let disposeBag = DisposeBag()
 
     override func viewDidLoad() {
         super.viewDidLoad()
         
         let nycCoordinates = CGPoint(x: 40.712784, y: -74.005941)
-        API.getCurrentWeatherForLocation(nycCoordinates)
+        API.getCurrentWeatherForLocation(nycCoordinates).subscribe(onNext: { temp in
+            print("temp obj: \(temp)")
+        }).addDisposableTo(disposeBag)
+//        API.getYesterdaysWeatherForLocation(nycCoordinates)
+    }
+}
+
+class WeatherDifferenceViewModel {
+    //get the difference - fires both api calls and returns with difference
+    
+    let weatherDelta = Variable(0.0)
+    
+    func updateWeatherDifferenceForLocation(location: CGPoint) {
+       //cool now we make the calls... wait for both to come back then update!
     }
 }
 
 struct API {
-    static func getCurrentWeatherForLocation(_ location: CGPoint) {
-       Alamofire.request(WeatherUndergroundRouter.getCurrentWeatherForLocation(location)).responseJSON { response in
+    
+    
+    
+    static func getCurrentWeatherForLocation(_ location: CGPoint) -> Observable<CurrentTemp> {
+        return Observable.create { observer -> Disposable in
+            Alamofire.request(WeatherUndergroundRouter.getCurrentWeatherForLocation(location)).responseObject(completionHandler: { (response: DataResponse<CurrentTemp>) in
+                if let results = response.result.value {
+                    observer.on(.next(results))
+                    observer.on(.completed)
+                }
+           })
+            return Disposables.create()
+        }
+    }
+    
+    static func getYesterdaysWeatherForLocation(_ location: CGPoint) {
+       Alamofire.request(WeatherUndergroundRouter.getYesterdaysWeatherForLocation(location)).responseJSON { response in
             if let JSON = response.result.value {
                 print("JSON: \(JSON)")
             }
         }
+    }
+//    static func getContentResultsForSection(_ sectionID: Int) -> Observable<[ContentResult]> {
+//        return Observable.create { observer -> Disposable in
+//            Alamofire.request(Router.getCategoryContent(sectionID)).responseCollection(completionHandler: { (response: DataResponse<[ContentResult]>) in
+//                if let results  = response.result.value {
+//                    observer.on(.next(results))
+//                    observer.on(.completed)
+//                }
+//            })
+//            return Disposables.create()
+//        }
+//    }
+}
+
+struct CurrentTemp {
+    let tempF: CGFloat
+}
+
+extension CurrentTemp: ResponseObjectSerializable {
+    init?(response: HTTPURLResponse, representation: Any) {
+        guard let innerRepresentation = representation as? [String: Any],
+        let currentObs = innerRepresentation["current_observation"] as? [String: Any],
+        let tempF = currentObs["temp_f"] as? CGFloat
+        else {
+           return nil
+        }
+        self.tempF = tempF
     }
 }
 
